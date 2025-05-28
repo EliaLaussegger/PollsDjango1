af22ae8cf53ef7903368531433688af5a73d5acd
EliaLaussegger <elia.laussegger@gmail.com>
2025-05-28 20:50:36 +0200

    login bei google jetz auch verifiziert

--- profiles/signals.py
+++ profiles/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from allauth.account.signals import user_logged_in
+from django.dispatch import receiver
+
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
@@ -16,3 +19,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+
