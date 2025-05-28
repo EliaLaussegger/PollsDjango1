98b7b651abcfc0ecfa1cd14accf475180f0e59a1
EliaLaussegger <elia.laussegger@gmail.com>
2025-05-28 19:53:19 +0200

    user profiles bug fix

--- profiles/signals.py
+++ profiles/signals.py
@@ -4,9 +4,14 @@ from django.dispatch import receiver
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+    else:
+        try:
+            instance.profile.save()
+        except Profile.DoesNotExist:
+            Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
